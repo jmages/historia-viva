3c8b490a99410dd8cb96a5974e09089f0bb0f6fa
jmages <jmages@gmx.de>
2015-02-07 18:14:32 +0100

    download index.html from historia-viva.net

--- main/java/jujumap/juju/TourView.java
+++ main/java/jujumap/juju/TourView.java
@@ -1,30 +1,45 @@
 package jujumap.juju;
 
+import android.app.Dialog;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TourView extends ListActivity {
 
     String path = "";
+    String url  = "";
+
+    private ProgressDialog pDialog;
+    public static final int progress_bar_type = 0;
 
     @Override
-    public void onCreate(Bundle bundle) {
+    public void onCreate (Bundle bundle) {
 
-        super.onCreate(bundle);
+        super.onCreate (bundle);
 
         Bundle b = getIntent().getExtras();
 
         path = b.getString("path");
+        url  = b.getString("url");
 
         setContentView(R.layout.tour_view);
 
@@ -39,15 +54,11 @@ public class TourView extends ListActivity {
 
         String selection = l.getItemAtPosition(position).toString();
 
-        Intent iData = new Intent();
+        Intent i = new Intent();
 
-        iData.putExtra(
-                "newPath",
-                selection);
+        i.putExtra ("newPath", selection);
 
-        setResult(
-                android.app.Activity.RESULT_OK,
-                iData );
+        setResult (RESULT_OK, i);
 
         this.finish();
     }
@@ -56,7 +67,7 @@ public class TourView extends ListActivity {
 
         List valueList = new ArrayList <String> ();
 
-        File[] files = new File(path).listFiles();
+        File [] files = new File(path).listFiles();
 
         for ( File aFile : files ) {
 
@@ -66,12 +77,127 @@ public class TourView extends ListActivity {
             }
         }
 
-        // Create a simple array adapter (of type string) with the test values
-        ListAdapter adapter =
-                new ArrayAdapter<String>(this,
-                        android.R.layout.simple_list_item_1,
-                        valueList);
+        new DownloadFileFromURL().execute(url);
+
+        ListAdapter adapter = new ArrayAdapter <String> (
+            this,
+            android.R.layout.simple_list_item_1,
+            valueList);
 
         return adapter;
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+
+        switch (id) {
+
+            case progress_bar_type: // we set this to 0
+
+                pDialog = new ProgressDialog(this);
+
+                pDialog.setMessage("Downloading file. Please wait...");
+                pDialog.setIndeterminate(false);
+                pDialog.setMax(100);
+                pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                pDialog.setCancelable(true);
+                pDialog.show();
+
+                return pDialog;
+
+            default:
+
+                return null;
+        }
+    }
+
+    class DownloadFileFromURL extends AsyncTask <String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+
+            showDialog(progress_bar_type);
+        }
+
+        @Override
+        protected String doInBackground(String... f_url) {
+
+            int count;
+
+            try {
+
+                URL url = new URL(f_url[0]);
+
+                URLConnection conection = url.openConnection();
+
+                conection.connect();
+
+                // this will be useful so that you can show a tipical 0-100%
+                // progress bar
+                int lenghtOfFile = conection.getContentLength();
+
+                // download the file
+                InputStream input = new BufferedInputStream(url.openStream(),
+                        8192);
+
+                // Output stream
+                OutputStream output = new FileOutputStream(Environment
+                        .getExternalStorageDirectory().toString()
+                        + "/2011.kml");
+
+                byte data[] = new byte[1024];
+
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    // publishing the progress....
+                    // After this onProgressUpdate will be called
+                    publishProgress("" + (int) ((total * 100) / lenghtOfFile));
+
+                    // writing data to file
+                    output.write(data, 0, count);
+                }
+
+                // flushing output
+                output.flush();
+
+                // closing streams
+                output.close();
+                input.close();
+
+            } catch (Exception e) {
+
+                Log.e("Error - no connection: ", e.getMessage());
+            }
+
+            return null;
+        }
+
+        /**
+         * Updating progress bar
+         * */
+        protected void onProgressUpdate(String... progress) {
+            // setting progress percentage
+            pDialog.setProgress(Integer.parseInt(progress[0]));
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        @Override
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after the file was downloaded
+            dismissDialog(progress_bar_type);
+
+        }
+
+        @Override
+        protected void onCancelled(){
+            // Handle what you want to do if you cancel this task
+        }
+
     }
 }
