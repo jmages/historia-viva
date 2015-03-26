7163d53876d41eddbc63f4fb7c7c32f0a69c7e94
jmages <jmages@gmx.de>
2015-03-26 06:50:42 +0100

    Track class cleared

--- main/java/jujumap/juju/Track.java
+++ main/java/jujumap/juju/Track.java
@@ -1,6 +1,5 @@
 package jujumap.juju;
 
-import android.util.Log;
 import org.osmdroid.util.BoundingBoxE6;
 
 import java.util.ArrayList;
@@ -15,9 +14,6 @@ public class Track extends ArrayList <TrackPoint> {
 
     float minAlt = 10000;
     float maxAlt = -1000;
-
-    long minTime = 1397630887000L;
-    long maxTime = 0;
 
     public void addPath (String rawPath) {
 
@@ -41,38 +37,9 @@ public class Track extends ArrayList <TrackPoint> {
         }
     }
 
-    public void addTP(double lat, double lon, double alt, float accuracy, double speed, double bearing, long time) {
-
-
-        if (time < minTime) minTime = time;
-        if (time > maxTime) maxTime = time;
-
-        TrackPoint tp = new TrackPoint (lat, lon, alt, accuracy, speed, bearing, time);
-
-        add(tp);
-    }
-
-    @Override
-    public String toString () {
-
-        StringBuilder sb = new StringBuilder();
-
-
-        for (TrackPoint tp : this) {
-
-            sb.append(tp.lon).append(",").append(tp.lat).append(",").append(tp.alt).append(",").
-            append(tp.accuracy).append(",").append(tp.speed).append(",").append(tp.bearing).append(",").
-            append(tp.time).append("\n");
-        }
-
-        return sb.toString();
-    }
-
     public BoundingBoxE6 get_bBox () {
 
-        BoundingBoxE6 bBox = new BoundingBoxE6 (maxLat, maxLon, minLat, minLon);
-
-        return bBox;
+        return new BoundingBoxE6 (maxLat, maxLon, minLat, minLon);
     }
 
     @Override
@@ -87,8 +54,5 @@ public class Track extends ArrayList <TrackPoint> {
 
         minAlt = 10000;
         maxAlt = -1000;
-
-        minTime = 1397630887000L;
-        maxTime = 0;
     }
 }
